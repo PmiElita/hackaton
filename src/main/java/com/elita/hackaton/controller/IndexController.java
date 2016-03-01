9315355bd7d35b1114d0824122cf0ec124d59a0c
Oleh <Pylypchak@ukr.net>
2016-03-01 22:14:04 +0200

    renamed package

--- src/main/java/com/elita/hackaton/controller/IndexController.java
+++ src/main/java/com/elita/hackaton/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.ekita.hackaton.controller;
+package com.elita.hackaton.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/elita/hackaton/controller/UserController.java
+++ src/main/java/com/elita/hackaton/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.ekita.hackaton.controller;
+package com.elita.hackaton.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
